3337503b25a4e9773159f19685e64b72a9ad8020
unsalmuhammed <avunsalmuhammed@gmail.com>
2025-07-08 18:10:39 +0300

    Composition

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,35 @@
 package org.example;
 
+import org.example.model.*;
+import org.example.model.enums.LampType;
+import org.example.model.enums.PaintColor;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Lamp lamp = new Lamp(LampType.NORMAL, true, 80 );
+        System.out.println(lamp);
+        Bed bed = new Bed("Çift kişilik", 20, 10, 5, 3);
+        System.out.println(bed);
+        Wardrobe wardrobe = new Wardrobe(20, 10, 50.5);
+        System.out.println(wardrobe);
+        Carpet carpet = new Carpet(3, 4, PaintColor.GREEN);
+        System.out.println(carpet);
+        Ceiling ceiling = new Ceiling(5, PaintColor.RED);
+        System.out.println(ceiling);
+        Wall wall = new Wall("left");
+        System.out.println(wall);
+
+        Ceiling ceiling2 = new Ceiling(3, PaintColor.WHITE);
+        Bed bed2 = new Bed("Queen", 2, 3, 1, 1);
+        Wall wall1 = new Wall("North");
+        Wall wall2 = new Wall("East");
+        Wall wall3 = new Wall("South");
+        Wall wall4 = new Wall("West");
+
+        Bedroom bedroom = new Bedroom("yatak odası", wall1, wall2, wall3, wall4, ceiling2, bed2, lamp, wardrobe, carpet);
+        System.out.println(bedroom);
+
     }
 }
---
+++ src/main/java/org/example/model/Bed.java
@@ -0,0 +1,51 @@
+package org.example.model;
+
+public class Bed {
+    private String style;
+    private int pillows;
+    private int height;
+    private int sheets;
+    private int quilts;
+
+    public Bed(String style, int pillows, int height, int sheets, int quilts) {
+        this.style = style;
+        this.pillows = pillows;
+        this.height = height;
+        this.sheets = sheets;
+        this.quilts = quilts;
+    }
+    public void make(){
+        System.out.println("The bed is being made.");
+    }
+
+    public String getStyle(){
+       return this.style = style;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getPillows() {
+        return pillows;
+    }
+
+    public int getSheets() {
+        return sheets;
+    }
+
+    public int getQuilts() {
+        return quilts;
+    }
+
+    @Override
+    public String toString() {
+        return "Bed{" +
+                "style='" + style + '\'' +
+                ", pillows=" + pillows +
+                ", height=" + height +
+                ", sheets=" + sheets +
+                ", quilt=" + quilts +
+                '}';
+    }
+}
